bbd57ea607a81f4c08577b79ea607c9f5a2154d3
asimbabayev99 <asim.babayev.99@bk.ru>
2020-03-25 12:55:50 +0400

    task model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TaskCategory(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=256)
+    category = models.ForeignKey(TaskCategory, on_delete=models.CASCADE)
+    location = models.CharField(max_length=256)
+    completed = models.BooleanField(default=False)
+
+    # all_day = models.BooleanField(default=False)
+    date = models.DateField(auto_now=True)
+    from_time = models.TimeField(auto_now=True)
+    to_time = models.TimeField(auto_now=True)
+
+    remind_me = models.DateTimeField(auto_now=True)
