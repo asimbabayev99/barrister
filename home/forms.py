652bfe943185929e227a7be11fc479e43f5371af
Soltanali-J <jafarlisoltanali@gmail.com>
2020-04-06 19:26:01 +0400

    add news

--- home/forms.py
+++ home/forms.py
@@ -5,7 +5,7 @@ from ckeditor import widgets
 
 class Newsform(forms.ModelForm):
     error_css_class = 'error'
-    title = forms.CharField(label='',max_length=50,min_length=5,widget=forms.TextInput(attrs={'class':'input','placeholder':'Basliq'}))
+    title = forms.CharField(label='',max_length=50,min_length=5,widget=forms.TextInput(attrs={'class':'form-group form-control','placeholder':'Xəbər başlığı'}))
     content = forms.CharField(label='',widget=widgets.CKEditorWidget(attrs={'class':'input'}))
     image = forms.ImageField(label='',required=False)
 
