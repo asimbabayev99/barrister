346075300398943968f16378c05a9619b9f6aba2
Soltanali-J <jafarlisoltanali@gmail.com>
2020-04-06 23:23:52 +0400

    news add styling

--- home/forms.py
+++ home/forms.py
@@ -5,9 +5,9 @@ from ckeditor import widgets
 
 class Newsform(forms.ModelForm):
     error_css_class = 'error'
-    title = forms.CharField(label='',max_length=50,min_length=5,widget=forms.TextInput(attrs={'class':'form-group form-control','placeholder':'Xəbər başlığı'}))
-    content = forms.CharField(label='',widget=widgets.CKEditorWidget(attrs={'class':'input'}))
-    image = forms.ImageField(label='',required=False)
+    title = forms.CharField(label='',max_length=50,min_length=5,widget=forms.TextInput(attrs={'class':'form-group form-control news-header','placeholder':'Xəbər başlığı'}))
+    content = forms.CharField(label='',widget=widgets.CKEditorWidget(attrs={'class':'form-group form-control'}))
+    image = forms.ImageField(label='',required=False,)
 
 
     class Meta:
