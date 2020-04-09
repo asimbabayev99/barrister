5a4f72c14e4979c67624147f1cbfdd32e10fa571
asimbabayev99 <asim.babayev.99@bk.ru>
2020-04-09 15:29:22 +0400

    admin add publication model

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from home.models import Event, EventCategory, News
+from home.models import Event, EventCategory, News, Publication
 
+admin.site.register(Publication)
 admin.site.register(Event)
 admin.site.register(EventCategory)
 admin.site.register(News)
