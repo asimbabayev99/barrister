084ef241f005097301360b1390e2daa84c971ef3
asimbabayev99 <asim.babayev.99@bk.ru>
2020-04-20 11:26:44 +0400

    bug fixed

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from account.views import *
-from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView, PasswordResetConfirmView , PasswordResetCompleteView , 
+from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView, PasswordResetConfirmView , PasswordResetCompleteView
 
 urlpatterns = [
     path('login/', login_view, name='login'),
