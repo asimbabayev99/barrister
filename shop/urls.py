40360b06b86f7d9611511cf3d4479b0fcbdaa66e
asimbabayev99 <asim.babayev.99@bk.ru>
2020-04-21 22:23:23 +0400

    shpo app

---
+++ shop/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from shop.views import *
+
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from barrister import settings
+
+urlpatterns = [
+    
+    path('', shop_view, name='shop'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
