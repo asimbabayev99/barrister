c54e73cec3f23d68c6c050854873d04110991475
Musa-Hesenli <musahesenli02@gmail.com>
2020-09-09 13:01:24 +0400

    dgfdgf

--- static/js/fullcalendar-appointments.js
+++ static/js/fullcalendar-appointments.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+          var cookies = document.cookie.split(';');
+          for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+            }
+          }
+        }
+        return cookieValue;
+      }
     $("#date_input").mask("xx/xx/xxxx");
     $("#time_input").mask("xx:xx");
     $("#date_input").datetimepicker({ format: 'DD/MM/YYYY', locale: 'az' });
