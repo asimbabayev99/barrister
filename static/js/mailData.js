6d7618e9af2608b2393bae9c221f170498f08c28
Musa-Hesenli <musahesenli02@gmail.com>
2020-09-29 10:30:05 +0400

    Many Changes

---
+++ static/js/mailData.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != "") {
+      var cookies = document.cookie.split(";");
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) == name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((json) =>{
+        json.forEach(element => {
+            $("#mailContent").append('<li class="list-group-item d-flex align-items-center"><div class="d-flex h-100 w-100 align-items-center justify-content-between"><span class="d-flex align-items-center"><input type="checkbox" name="" id=""><span class="emailNameTitle">' + element.username +'</span><span class="emailSubject">' + element.name +'</span><span class="emailContentShort">' + element.company.catchPhrase +'</span></span><span>' + element.id + '</span></li>'   )
+        });
+    });
+  fetch("emails/")
+  .then((response) => response.json())
+  .then((json) => {
+      console.log(json)
+  })
+  
+});
