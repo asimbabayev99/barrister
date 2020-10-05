0c1a6a32fd07156ace58957abf0e2fa99842f72c
azad-mammedov <azad.mammedov6@gmail.com>
2020-10-05 12:48:17 +0400

    a

--- barrister/settings.py
+++ barrister/settings.py
@@ -92,12 +92,14 @@ DATABASES = {
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'barrister',
-        'USER': 'barrister',
-        'PASSWORD': 'barrister',
-        'HOST': 'vmi388731.contaboserver.net',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':'db.sqlite3'
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'barrister',
+        # 'USER': 'barrister',
+        # 'PASSWORD': 'barrister',
+        # 'HOST': 'vmi388731.contaboserver.net',
+        # 'PORT': '5432',
     }
 }
 
