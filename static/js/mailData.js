3f6ca796b54eefeb94fccd398f1667b072d5ec90
Musa-Hesenli <musahesenli02@gmail.com>
2020-10-16 15:19:53 +0400

    aZAD BESBANTOV

--- static/js/mailData.js
+++ static/js/mailData.js
@@ -20,12 +20,14 @@ $(document).ready(function () {
         $(".mailListGroup").click(function(){
           console.log($(this).attr("id"));
           var mail = $(this).attr("id");
+         
           json.forEach(element => {
             if(element.id == mail) {
+              // $("#" + mail).css("background","#007bff");
               $(".mailSubject").text(element.sender);
               $(".mailSender").text(element.sender);
               $(".mailDate").text(element.date);
-              $(".mailMovzu").text(element.content)
+              $(".mailMovzu").html(element.content)
 
             }
           })
