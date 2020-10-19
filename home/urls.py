12de06926668f424b89f174b2744c95d899531b0
azad-mammedov <azad.mammedov6@gmail.com>
2020-10-19 15:28:43 +0400

    a

--- home/urls.py
+++ home/urls.py
@@ -49,7 +49,7 @@ urlpatterns = [
 
 
     re_path(r'^media/attachment/(?P<path>.*)', attachment_media_download, name='attachment-media'),
-    re_path(r'view/(?P<path>.*)',attachment_media_download)
+    # re_path(r'view/(?P<path>.*)',attachment_media_download)
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
