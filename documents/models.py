d9d9828beb006b2c0662db0dc06d35586bb8ceb6
asimbabayev99 <asim.babayev.99@bk.ru>
2020-10-19 16:36:15 +0400

    document model

--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    template = models.FileField(upload_to='documents', null=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
