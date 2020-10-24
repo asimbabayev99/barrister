5bf13d3672e85753992b20e8218a05b76dc968ea
Musa-Hesenli <musahesenli02@gmail.com>
2020-10-24 14:09:35 +0400

    ,m

--- static/js/mailData.js
+++ static/js/mailData.js
@@ -46,6 +46,7 @@ $(document).ready(function () {
                 json.forEach(element => {
                   if(element.id == mail) {
                     $(".mailSubject").text(element.sender);
+                    $(".mailSubject").attr("id", element.id)
                     $(".mailSender").text(element.sender);
                     $(".mailSubjectExpand").text(element.subject)
                     let time = element.date.split("-");
@@ -62,6 +63,7 @@ $(document).ready(function () {
                     })          
                   }
                 })
+            
                          
               });
             }
@@ -146,24 +148,31 @@ $(document).ready(function () {
             if(element.folder === "Trash") {
               $("#mailContent").append('<li num='+ element.num +' id="'+ element.id +'" class="list-group-item d-flex align-items-center mailListGroup"><div class="d-flex h-100 w-100 align-items-center justify-content-between"><span class="d-flex align-items-center"><input type="checkbox" class="checkBoxMails" name="'+ element.folder +'" id="mailCheckboxes"><span class="emailNameTitle">' + element.sender +'</span><span class="emailSubject">' + element.subject +'</span></span></li>');
               $(".mailListGroup").click(function(){
+                $(".mailSubject").html('<div class="spinner-grow text-dark" role="status"><span class="sr-only">Loading...</span></div>');
+                $(".mailSender").html('<div class="spinner-grow spinner-grow-sm text-dark" role="status"><span class="sr-only">Loading...</span></div>')
+                
+                $(".mailSubjectExpand").html('')
+                $(".mailDate").html('');
+                $(".mailMovzu").html('');
+                setTimeout(()=> {
                 $(".defaultMail").show(200);
                 $(".insteadMailImage").hide(100)
-                
                 var mail = $(this).attr("id");
-                
                 json.forEach(element => {
                   if(element.id == mail) {
+                    
+                    
                     $(".mailSubject").text(element.sender);
                     $(".mailSender").text(element.sender);
                     $(".mailSubjectExpand").text(element.subject)
+                    $(".mailDate").text(wholeTime);
+                    $(".mailMovzu").html(element.content);
                     let time = element.date.split("-");
                     let endTime = time[2].split("");
                     let wholeTime = time[0] + "-" + time[1]+ "-" + endTime[0] + "" + endTime[1]
-                    $(".mailDate").text(wholeTime);
-                    $(".mailMovzu").html(element.content);
                     let mailSender = element.sender.split('');
                     let color = ["purple","blue","green","maroon","darkorange"];
-                    $(".emailImage").text(mailSender[0].toUpperCase() + '' + mailSender[1].toUpperCase());
+                    $(".emailImage").html(mailSender[0].toUpperCase() + '' + mailSender[1].toUpperCase());
                     let rNumber = Math.floor(Math.random()*5);
                     
                     $(".emailImage").css({
@@ -171,6 +180,7 @@ $(document).ready(function () {
                     })          
                   }
                 })
+                },1000)
                          
               });
             }
@@ -204,6 +214,7 @@ $(document).ready(function () {
           json.forEach(element => {
             if(element.id == mail) {
               $(".mailSubject").text(element.sender);
+              $(".mailSubject").attr("id",mail)
               $(".mailSender").text(element.sender);
               $(".mailSubjectExpand").text(element.subject)
               let time = element.date.split("-");
@@ -212,15 +223,12 @@ $(document).ready(function () {
               $(".mailDate").text(wholeTime);
               $(".mailMovzu").html(element.content);
               let mailSender = element.sender.split('');
-              let color = ["purple","blue","green","maroon","darkorange"];
               $(".emailImage").text(mailSender[0].toUpperCase() + '' + mailSender[1].toUpperCase());
-              let rNumber = Math.floor(Math.random()*5);
               
-              $(".emailImage").css({
-                backgroundColor : color[rNumber]
-              })          
             }
+
           })
+          
         var deletingMessagesIds = []
         var folder;
         
