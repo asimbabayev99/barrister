a3d9e20367f97ceeff2266c6e698e6345c09828a
asimbabayev99 <asim.babayev.99@bk.ru>
2020-10-25 16:07:53 +0400

    chat

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Q
+from chat.models import Message, Attachment
 # Create your views here.
 
 
@@ -8,8 +9,10 @@ from django.contrib.auth.decorators import login_required
 def index(request, user_id):
 
     room_name = str(min(request.user.id, user_id)) + "-" + str(max(request.user.id, user_id))
+    messages = Message.objects.filter(Q(sender=request.user) | Q(receiver=request.user)).order_by('date')
 
     context = {
+        'messages': messages,
         'room_name': room_name,
     }
 
