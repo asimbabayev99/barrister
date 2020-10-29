3323f4224083b2d9b972253a6931004c44974d71
asimbabayev99 <asim.babayev.99@bk.ru>
2020-10-29 15:38:31 +0400

    frame

--- chat/consumers.py
+++ chat/consumers.py
@@ -18,6 +18,28 @@ def save_message(**data):
     return msg
 
 
+@database_sync_to_async
+def delete_message(**data):
+    id = data.get('id')
+    try:
+        msg = Message.objects.get(id=id)
+    except:
+        return False
+    if msg.sender == data.get('sender') and msg.receiver == data.get('receiver'):
+        msg.delete()
+        return True
+    else:
+        return False
+    
+
+@database_sync_to_async
+def save_attachment(**data):
+    message_id = data.get('message_id')
+    name = data.get('name')
+    upload_file = data.get('upload_file')
+    # attachment = Attachment(message_id=message_id, file=upload_file)
+    # return attachment
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -44,26 +66,110 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         message_type = data.get('message_type')
-        message = data['message']
+        message_action = data.get('message_action')
+
         receiver = self.room_name.replace(str(self.scope['user'].id), '').replace('-', '')
         receiver = CustomUser.objects.get(id=int(receiver))
         sender = self.scope['user']
 
-        # Store message.
-        msg = await save_message(message=message, sender=sender, receiver=receiver)
+        # if message type is text
+        if message_type == 'text':
+            if message_action == 'post':
+                message = data['message']
+                # Store message.
+                msg = await save_message(message=message, sender=sender, receiver=receiver)
+            elif message_action == "delete":
+                message = ""
+                id = data['id']
+                result = await delete_message(id=id, sender=sender, receiver=receiver)
+                if not result:
+                    await self.channel_layer.group_send(
+                        self.room_group_name,
+                        {
+                            'type': 'handle_error',
+                            'error_message': 'Exception occured during deleting message'
+                        }
+                    )   
+            else:
+                # if wrong message action is wrong then send error message
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'handle_error',
+                        'error_message': 'Exception. message_type action be one of these: post, delete'
+                    }
+                )
 
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message_type': message_type,
-                'message': message,
-                'date': datetime.strftime(msg.date, '%d.%m.%Y %H:%M:%S'),
-                'sender': sender.id,
-                'receiver': receiver.id
-            }
-        )
+            # Send message to room group
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message_type': message_type,
+                    'message_action': message_action,
+                    'id': msg.id,
+                    'message': message,
+                    'date': datetime.strftime(msg.date, '%d.%m.%Y %H:%M:%S'),
+                    'sender': sender.id,
+                    'receiver': receiver.id
+                }
+            )
+        # if message type is file
+        elif message_type == "file":
+            if message_action == "post":
+                self.session = {
+                    # Set up file object and attributes
+                    'upload_file': '',
+                    'name': '',
+                    'size': ''
+                }
+
+            elif message_action == "delete":
+                pass
+            else:
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'handle_error',
+                        'error_message': 'Exception. message_type be one of these: post, delete'
+                    }
+                )
+        else: 
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'handle_error',
+                    'error_message': 'Exception. message_action  be one of these: text, file'
+                }
+            )
+                
+
+    async def handle_chunk(self, message, **kwargs):
+        upload_size = self.session.get('upload_size')
+        temp_destination = self.session.get('upload_file')
+
+        if not upload_size or not temp_destination:
+            return self.error('Invalid request. Please try again.')
+
+        self.session['upload_file'].write(message)
+        size = self.session['upload_file'].tell()
+
+        percent = round((size / upload_size) * 100)
+        await self.send_json({
+            'action': 'progress',
+            'percent': percent,
+            'file_size': size
+        })
+
+        if size >= upload_size:
+            self.session['upload_file'].flush()
+            file_name = await self.handle_complete(self.session['upload_file'])
+
+            await self.send_json({
+                'action': 'complete',
+                'file_size': size,
+                'file_name': file_name
+            }, close=True)
         
     
     async def chat_message(self, event):
@@ -75,9 +181,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
+            'success': True,
             'message': message,
             'message_type': message_type,
             'date': date,
             'sender': sender,
             'receiver': receiver
+        }))
+
+    
+    async def handle_error(self, event):
+        error_message = event['error_message']
+
+        await self.send(text_data=json.dumps({
+            'success': 'False',
+            'error_message': error_message,
         }))
--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from chat.models import Message, Attachment
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -9,7 +10,7 @@ from chat.models import Message, Attachment
 def index(request, user_id):
 
     room_name = str(min(request.user.id, user_id)) + "-" + str(max(request.user.id, user_id))
-    messages = Message.objects.filter(Q(sender=request.user) | Q(receiver=request.user)).order_by('date')
+    messages = Message.objects.filter(Q(sender=request.user) & Q(receiver_id=user_id) | Q(receiver=request.user) & Q(sender_id=user_id)).order_by('date')
 
     context = {
         'messages': messages,
